37b38ef932afd6a0097a4671f24953d98d085230
Philipp Möller <bootsarehax@gmail.com>
2016-01-07 15:51:09 +0100

    Create logs to actually create the testsuite

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 if [ "$1" = 'cgal_build_documentation' ]; then
     mkdir /cgal_build
     cd /cgal_build
-    cmake -DBUILD_DOC:BOOL=ON /mnt/cgal
+    cmake -DBUILD_DOC:BOOL=ON \
+          -DCGAL_DOC_CREATE_LOGS:BOOL=ON \
+          /mnt/cgal
     make doc
     make Documentation_test
 else
